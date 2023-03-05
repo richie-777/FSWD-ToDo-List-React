aacf2fe00072b15624fefe914c7eb2b982347e14
richie777 <79468696+richie-777@users.noreply.github.com>
2023-03-05 11:25:40 +0530

    Add files via upload

---
+++ src/components/ListItems.js
@@ -0,0 +1,31 @@
+import "../components/ListItems.css";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function ListItems(props) {
+  const items = props.items;
+  const listitems = items.map((item) => {
+    return (
+      
+      <div className="list" key={"item.Key"}>
+        <p>
+          <input type="text" id={item.key} value={item.text} onChange={
+            (e)=>{
+              props.setUpdate(e.target.value,item.key)
+            }
+          }/>
+          <span>
+            <FontAwesomeIcon
+              className="faicons"
+              icon="trash"
+              onClick={() => props.deleteItem(item.key)}
+            />
+          </span>
+        </p>
+      </div>
+      
+    );
+  });
+  return <div>{listitems}</div>;
+}
+export default ListItems;
